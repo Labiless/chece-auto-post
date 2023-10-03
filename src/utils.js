aca485f75bf9da86c0502388134a0d255493384c
Labiles <52052455+Labiless@users.noreply.github.com>
2023-10-03 12:25:51 +0200

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export const getDate = () => {
     const date = new Date();
     const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
-    const month = date.getMonth() < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+    const month = date.getMonth() < 9 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
     const year = date.getFullYear();
     return `${day}/${month}/${year}`;
 }
